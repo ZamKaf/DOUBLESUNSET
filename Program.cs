79950b61ae75ad2048cf4a084ecca6fb2fa60e48
Cignid3001 <71765164+Cignid3001@users.noreply.github.com>
2020-10-03 19:10:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DOUBLESYSTEM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num < 2):
+
+
+        }
+    }
+}
